fa58562f137412d894cd0a5c92d214b74b34b52a
unknown <prashanthkusa123@gmail.com>
2024-10-16 21:44:22 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from './Components/Table';
+import './App.css';
+
+const App = () => (
+  <div className="game">
+    <div className="game-board">
+      <Table />
+    </div>
+  </div>
+);
+
+export default App;
